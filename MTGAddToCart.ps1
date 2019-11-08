fbb57a78ea53b3a1b5b296ff847528509da8efa9
kzalewski11 <49792318+kzalewski11@users.noreply.github.com>
2019-11-08 14:30:04 -0800

    Add files via upload
    Initial commit of two existing powershell scripts

---
+++ MTGAddToCart.ps1
@@ -0,0 +1,17 @@
+﻿#get clipboard and edit
+(Get-Clipboard | select -Skip 1 | % {$_ -replace " \(\w*\) \d*",""}) | Set-Clipboard
+
+#open website
+Start-Process -FilePath "https://store.tcgplayer.com/massentry"
+#$w1.visible=$true
+
+#sleep 3 secs (hackily wait for website to load)
+Start-Sleep -s 3
+
+#paste into window
+[System.Windows.Forms.SendKeys]::SendWait("{TAB 16}")
+[System.Windows.Forms.SendKeys]::SendWait("^{v}")
+
+#add to cart
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
---
+++ MTGremoveUselessStuff.ps1
@@ -0,0 +1,2 @@
+﻿$file = Read-Host -Prompt 'File Name: '
+(gc $file | select -Skip 1 | % {$_ -replace " \(\w*\) \d*",""}) | sc $file
